2f8b3be47b8d5a0195d847835364382173e558c3
aliev <davlatbek.mamurjonov@newproweb.com>
2026-03-27 11:51:54 +0500

    Update package version to 5.7.21 and enhance TransactionTable with toggleable column visibility feature

--- admin/src/pages/App/components/transaction-history/TransactionTable.jsx
+++ admin/src/pages/App/components/transaction-history/TransactionTable.jsx
@@ -1,5 +1,13 @@
 import * as React from "react";
-import { Typography, Box, Button, Flex } from "@strapi/design-system";
+import {
+  Typography,
+  Box,
+  Button,
+  Flex,
+  Checkbox,
+  SimpleMenu,
+  MenuItem,
+} from "@strapi/design-system";
 import { Table, Pagination } from "@strapi/strapi/admin";
 import { ChevronDownIcon, ChevronUpIcon } from "../icons";
 import {
@@ -41,6 +49,144 @@ const TransactionTable = () => {
     [t]
   );
 
+  const [visibleColumns, setVisibleColumns] = React.useState(() =>
+    headers.reduce((acc, col) => {
+      acc[col.name] = true;
+      return acc;
+    }, {})
+  );
+
+  React.useEffect(() => {
+    setVisibleColumns((prev) =>
+      headers.reduce((acc, col) => {
+        acc[col.name] = prev[col.name] ?? true;
+        return acc;
+      }, {})
+    );
+  }, [headers]);
+
+  const toggleableHeaders = React.useMemo(
+    () => headers.filter((header) => header.name !== "details"),
+    [headers]
+  );
+
+  const visibleHeaders = React.useMemo(
+    () => headers.filter((header) => visibleColumns[header.name]),
+    [headers, visibleColumns]
+  );
+
+  const visibleColumnCount = Math.max(visibleHeaders.length, 1);
+
+  const toggleColumn = (key) => {
+    if (key === "details") {
+      return;
+    }
+
+    setVisibleColumns((prev) => {
+      const currentlyVisibleToggleableCount = toggleableHeaders.filter(
+        (header) => prev[header.name]
+      ).length;
+
+      const isTryingToHideLastToggleableColumn =
+        prev[key] && currentlyVisibleToggleableCount === 1;
+
+      if (isTryingToHideLastToggleableColumn) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        [key]: !prev[key],
+      };
+    });
+  };
+
+  const showAllColumns = () => {
+    setVisibleColumns(
+      headers.reduce((acc, col) => {
+        acc[col.name] = true;
+        return acc;
+      }, {})
+    );
+  };
+
+  const renderCellContent = (headerName, transaction, isSelected) => {
+    switch (headerName) {
+      case "txid":
+        return (
+          <Typography variant="pi" textColor="neutral600">
+            {transaction.txid || t("table.na", "N/A")}
+          </Typography>
+        );
+      case "reference":
+        return (
+          <Typography variant="pi" fontWeight="medium">
+            {transaction.reference || t("table.na", "N/A")}
+          </Typography>
+        );
+      case "amount":
+        return (
+          <Typography variant="pi" textColor="neutral600">
+            {formatAmount(transaction.amount, transaction.currency)}
+          </Typography>
+        );
+      case "paymentMethod":
+        return (
+          <Typography variant="pi">
+            {getPaymentMethodName(
+              transaction.raw_request?.clearingtype ||
+                transaction.body?.raw_request?.clearingtype,
+              transaction.raw_request?.wallettype ||
+                transaction.body?.raw_request?.wallettype,
+              transaction.raw_request?.cardtype ||
+                transaction.body?.raw_request?.cardtype
+            )}
+          </Typography>
+        );
+      case "type":
+        return (
+          <Typography variant="pi" fontWeight="semiBold">
+            {transaction.request_type || transaction.body?.request_type || t("table.na", "N/A")}
+          </Typography>
+        );
+      case "status":
+        return (
+          <StatusBadge
+            status={transaction.status || transaction.body?.status}
+            transaction={transaction}
+          />
+        );
+      case "created_at":
+        return (
+          <Typography variant="pi" textColor="neutral600">
+            {formatDate(transaction.created_at ?? transaction.createdAt)}
+          </Typography>
+        );
+      case "updated_at":
+        return (
+          <Typography variant="pi" textColor="neutral600">
+            {formatDate(transaction.updated_at ?? transaction.updatedAt)}
+          </Typography>
+        );
+      case "details":
+        return (
+          <Button
+            size="S"
+            variant="tertiary"
+            minWidth="100px"
+            onClick={() => handleTransactionSelect(transaction)}
+            startIcon={
+              isSelected ? <ChevronUpIcon size={16} /> : <ChevronDownIcon size={16} />
+            }
+          >
+            {isSelected ? t("table.hide", "Hide") : t("table.details", "Details")}
+          </Button>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <Flex direction="column" alignItems="stretch" gap={4} minHeight={"800px"}>
       <FiltersPanel
@@ -49,15 +195,55 @@ const TransactionTable = () => {
         loadTransactionHistory={loadTransactionHistory}
       />
 
+      <Flex
+        justifyContent="flex-end"
+        gap={3}
+        alignItems="stretch"
+        marginBottom={2}
+        marginTop={4}
+      >
+        <SimpleMenu
+          label={t("table.actions", "Actions")}
+          id="transaction-table-actions-menu"
+          onReachEnd={() => {}}
+          variant="secondary"
+        >
+          {toggleableHeaders.map((header) => (
+            <MenuItem
+              key={header.name}
+              to={undefined}
+              href={undefined}
+              onClick={() => toggleColumn(header.name)}
+            >
+              <Flex alignItems="center" gap={2}>
+                <Checkbox
+                  name={`column-${header.name}`}
+                  checked={visibleColumns[header.name]}
+                  onCheckedChange={() => toggleColumn(header.name)}
+                />
+                <span>{header.label}</span>
+              </Flex>
+            </MenuItem>
+          ))}
+          <MenuItem
+            to={undefined}
+            href={undefined}
+            onClick={showAllColumns}
+          >
+            {t("table.showAllColumns", "Show all columns")}
+          </MenuItem>
+        </SimpleMenu>
+      </Flex>
+
       <Box>
         <Table.Root
           rows={transactions}
-          headers={headers}
+          headers={visibleHeaders}
           isLoading={isLoadingHistory}
         >
           <Table.Content>
             <Table.Head>
-              {headers.map((header) => (
+              {visibleHeaders.map((header) => (
                 <Table.HeaderCell
                   key={header.name}
                   name={header.name}
@@ -74,71 +260,15 @@ const TransactionTable = () => {
                   return (
                     <React.Fragment key={transaction.id}>
                       <Table.Row>
-                        <Table.Cell>
-                          <Typography variant="pi" textColor="neutral600">
-                            {transaction.txid || t("table.na", "N/A")}
-                          </Typography>
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Typography variant="pi" fontWeight="medium">
-                            {transaction.reference || t("table.na", "N/A")}
-                          </Typography>
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Typography variant="pi" textColor="neutral600">
-                            {formatAmount(
-                              transaction.amount,
-                              transaction.currency
-                            )}
-                          </Typography>
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Typography variant="pi">
-                            {getPaymentMethodName(
-                              (transaction.raw_request?.clearingtype || transaction.body?.raw_request?.clearingtype),
-                              (transaction.raw_request?.wallettype || transaction.body?.raw_request?.wallettype)
-                            )}
-                          </Typography>
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Typography variant="pi" fontWeight="semiBold">
-                            {transaction.request_type || t("table.na", "N/A")}
-                          </Typography>
-                        </Table.Cell>
-                        <Table.Cell>
-                          <StatusBadge status={transaction?.status} transaction={transaction} />
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Typography variant="pi" textColor="neutral600">
-                            {formatDate(transaction.created_at ?? transaction.createdAt)}
-                          </Typography>
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Typography variant="pi" textColor="neutral600">
-                            {formatDate(transaction.updated_at ?? transaction.updatedAt)}
-                          </Typography>
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Button
-                            size="S"
-                            variant="tertiary"
-                            minWidth="100px"
-                            onClick={() => handleTransactionSelect(transaction)}
-                            startIcon={
-                              isSelected ? (
-                                <ChevronUpIcon size={16} />
-                              ) : (
-                                <ChevronDownIcon size={16} />
-                              )
-                            }
-                          >
-                            {isSelected ? t("table.hide", "Hide") : t("table.details", "Details")}
-                          </Button>
-                        </Table.Cell>
+                        {visibleHeaders.map((header) => (
+                          <Table.Cell key={`${transaction.id}-${header.name}`}>
+                            {renderCellContent(header.name, transaction, isSelected)}
+                          </Table.Cell>
+                        ))}
                       </Table.Row>
                       {isSelected && (
                         <Table.Row>
-                          <Table.Cell colSpan={10}>
+                          <Table.Cell colSpan={visibleColumnCount}>
                             <TransactionDetails transaction={transaction} />
                           </Table.Cell>
                         </Table.Row>
